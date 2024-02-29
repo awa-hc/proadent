13b1a8b95b5a54b7184a0cefbf43f8d59f4e4db9
Horacio Cortez <leohermoso18@gmail.com>
2024-02-29 00:45:06 -0300

    change color of angular material
    change color to pink

--- front/app/src/app/app.config.ts
+++ front/app/src/app/app.config.ts
@@ -13,6 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimationsAsync(),
     provideServerRendering(),
-    provideAnimations(),
+    provideAnimations(), provideAnimationsAsync(),
   ],
 };
--- front/app/src/app/pages/login/login.component.ts
+++ front/app/src/app/pages/login/login.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatLabel,
+    MatCardModule,
+    MatCard,
+    MatFormField,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log('Formulario enviado');
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
